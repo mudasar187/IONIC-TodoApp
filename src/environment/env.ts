e8588a04ba15e18d4375fa50add8e13c1882bf74
Mudasar Ahmad <ahmmud16@hotmail.com>
2018-03-01 19:33:08 +0100

    Delete env.ts

--- src/environment/env.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "AIzaSyCiK3XQof5XfRi62zp7ICb1stkMWcBbMxc",
-  authDomain: "todoapp-a910d.firebaseapp.com",
-  databaseURL: "https://todoapp-a910d.firebaseio.com",
-  projectId: "todoapp-a910d",
-  storageBucket: "todoapp-a910d.appspot.com",
-  messagingSenderId: "662869990208"
-};
