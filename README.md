9e0e16d2d15daa5e6550ca5ea64092d26676509d
mudasar187 <ahmmud16@hotmail.com>
2018-03-22 16:33:32 +0100

    added readme.md file

---
+++ README.md
@@ -0,0 +1,30 @@
+# IONIC - TodoApp
+
+A TodoApp by using IONIC
+
+## Run
+
+You'll need:
+
+- [Firestore](https://firebase.google.com/)
+- [IONIC](https://ionicframework.com/)
+- [Node.js](https://nodejs.org/en/)
+- [NPM](https://www.npmjs.com/)
+
+Do:
+
+- Create an account on [Firestore](https://firebase.google.com/) , then add a new project and then go to a symbol like this '</>', click there and get the api key, and make a file inside 'environment' folder called 'env.ts' and paste the key inside there like this:
+
+export default { <br />
+  apiKey: "-----------------------------------",<br />
+  authDomain: "-----------------------------------",<br />
+  databaseURL: "-----------------------------------",<br />
+  projectId: "-----------------------------------",<br />
+  storageBucket: "-----------------------------------",<br />
+  messagingSenderId: "-----------------------------------"<br />
+};
+
+On command line:
+
+- npm install (Install all dependencies you need)
+- ionic serve --port 1234
--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -10,11 +10,6 @@ import { Network } from '@ionic-native/network';
 })
 export class WelcomePage {
 
-  /*
-  connected: Subscription;
-  disconnected: Subscription;
-  */
-
   constructor(public navCtrl: NavController, private emailComposer: EmailComposer, private network: Network, private toast: ToastController) {
    /* this.emailComposer.isAvailable().then((available: boolean) => {
       if (available) {
@@ -22,36 +17,6 @@ export class WelcomePage {
       }
     }); */
   }
-
-
-  /*
-  displayNetworkUpdate(connectionState: string){
-    let networkType = this.network.type;
-    this.toast.create({
-      message: `You are now ${connectionState} via ${networkType}`,
-      duration: 3000
-    }).present();
-  }
-
-  ionViewDidEnter() {
-    this.connected = this.network.onConnect().subscribe(data => {
-      console.log(data)
-      this.displayNetworkUpdate(data.type);
-    }, error => console.error(error));
-
-    this.disconnected = this.network.onDisconnect().subscribe(data => {
-      this.navCtrl.push('NetworkOfflinePage');
-      console.log(data)
-      this.displayNetworkUpdate(data.type);
-    }, error => console.error(error));
-  }
-
-  ionViewWillLeave(){
-    this.navCtrl.push('NetworkOfflinePage');
-    this.connected.unsubscribe();
-    this.disconnected.unsubscribe();
-  }
-  */
 
   goToResetPasswordPage() {
     this.navCtrl.push('ResetPasswordPage')
